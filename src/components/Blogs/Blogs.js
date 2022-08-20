d8a721934c2df9524bfddca5c002daa70e0c6b6d
SandeepSokle <sandeep.kumar@nirmitee.io>
2022-08-20 21:05:33 +0530

    up

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,7 +1,7 @@
 // import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import {  useState } from "react";
+// import { styled } from "@mui/material/styles";
+// import Paper from "@mui/material/Paper";
+// import {  useState } from "react";
 
 
 // const Item = styled(Paper)(({ theme }) => ({
